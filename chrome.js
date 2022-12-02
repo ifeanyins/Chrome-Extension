81d416f803dff302d1fff68699c9a4e2b97d8ce8
ifeanyi_ns <ifeanyi_ns@icloud.com>
2022-12-02 01:01:42 +0100

    styling and addition of event listener

--- chrome.js
+++ chrome.js
@@ -1,1 +1,31 @@
+let inputBtn = document.getElementById('input-btn')
+inputBtn.addEventListener('click', let  =()=> {
+    console.log('click from ns')
+})
+
+
+
+
+
+
+
+
+
+
 // document.title = "CHrome Extension"
+
+
+
+
+
+
+
+
+
+
+
+
+let openBox = document.getElementById('box')
+openBox.addEventListener('click', let =()=> {
+    console.log('i want to open the box')
+})
