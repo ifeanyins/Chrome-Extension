35147e7fc2e8c933236971020c87f2bd3eea9dec
ifeanyi_ns <ifeanyi_ns@icloud.com>
2022-12-04 06:51:41 +0100

    finalisation of the extension

--- chrome.js
+++ chrome.js
@@ -1,31 +1,24 @@
 let myLeads = [];
+let oldleads = [];
 const ulEl = document.querySelector('#ul-el')
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+const deleteBtn = document.querySelector('#delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
-
-inputBtn.addEventListener('click', let  =()=> {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-    inputEl.value = ''
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-})
-
-function renderLeads (){
-
+function render (leads){
     let listItems = '';
-    for (let i = 0; i < myLeads.length; i++){
+    for (let i = 0; i < leads.length; i++){
        listItems += `
        <li>
-         <a target='_blank' href ='${myLeads[i]}'>
-            ${myLeads[i]}
-          </a>
+         <a target='_blank' href ='${leads[i]}'>
+             ${leads[i]}
+         </a>
        </li>
        `
         // const li = document.createElement('li')
@@ -35,11 +28,32 @@ function renderLeads (){
     ulEl.innerHTML = listItems
 }
 
+tabBtn.addEventListener('click', function(){
+    chrome.tabs.query({active:true, currentWindow: true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify (myLeads))
+        render(myLeads)
+    }) 
+})
+
+deleteBtn.addEventListener ('dblclick', function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener('click', let  =()=> {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+
 
 
 
 
 
 document.title = "Chrome Extension"
-
-
